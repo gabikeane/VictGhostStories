12bd46acf47f47be88885e150b3c4bb7682d3b0a
amd212 <amd212@pitt.edu>
2016-12-16 18:58:27 -0500

    Everything for Javascript

---
+++ html/VGdropdown.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+    var buttons = document.getElementsByTagName("button");
+    for (i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+  /*      button.addEventListener('click',function() {
+     show_hide();
+     buttonColor();
+});*/
+          button.addEventListener('click', show_hide, false);
+        
+    }
+ 
+}
+
+
+function show_hide() {
+
+  
+  if (this.style.backgroundColor != 'transparent') {
+    this.style.backgroundColor = 'transparent';
+  } else {
+    this.style.backgroundColor = '';
+  }
+
+	var matchingElements = document.getElementsByClassName(this.className);
+	for (i = 0; i <matchingElements.length; i++) {
+		console.log("in show_hide for");
+
+		if (matchingElements[i] != this) {
+			console.log("in not this");
+			if (matchingElements[i].style.display == 'block') {
+		        matchingElements[i].style.display = 'none';
+		        console.log("unhide");
+   			} else {
+		        matchingElements[i].style.display = 'block';
+			}
+		}
+	}
+}
